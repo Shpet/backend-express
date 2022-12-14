7abd6b377b404a83c5bee42aa366164d5c0da63b
Vadym Shpet <vadik.shpet01@gmail.com>
2022-12-14 17:00:47 +0200

    FEATURE: added js doc

--- src/DTO/AddFruitModel.ts
+++ src/DTO/AddFruitModel.ts
@@ -1,6 +1,21 @@
 export type AddFruitModel = {
+  /**
+   * id of existing fruit
+   */
   id: number
+
+  /**
+   * name of fruit
+   */
   name: string
+
+  /**
+   * color fruit
+   */
   color?: string
+
+  /**
+   * fruit price
+   */
   price?: number
 }
--- src/DTO/InfoMessageModel.ts
+++ src/DTO/InfoMessageModel.ts
@@ -1,3 +1,6 @@
 export type InfoMessageModel = {
+  /**
+   * text information
+   */
   message: string
 }
